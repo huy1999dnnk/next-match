cc8e823966db7403f27e5b9292384962d303c76f
pham minh huy <phamminhhuy@phams-Laptop.local>
2024-12-07 14:49:38 +0700

    done table message feature

--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -1,5 +1,5 @@
 import { getMemberByUserId } from '@/app/actions/memberActions'
-import { CardBody, CardHeader, Divider } from '@nextui-org/react';
+import CardInnerWrapper from '@/components/CardInnerWrapper';
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -11,14 +11,9 @@ export default async function MemberDetailedPage({ params }: { params: { userId:
   }
 
   return (
-    <>
-      <CardHeader className='text-2xl font-semibold text-secondary'>
-        Profile
-      </CardHeader>
-      <Divider />
-      <CardBody>
-        {member.description}
-      </CardBody>
-    </>
+    <CardInnerWrapper
+      header='Profile'
+      body={<div>{member.description}</div>}
+    />
   )
 }
